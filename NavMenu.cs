c7ecc46fdca7d637f3111c6fb3990bb14a4aba89
Richard Trygg Svartling <tryggrichard@gmail.com>
2025-01-03 12:24:46 +0100

    la till några placeholders för menyn

---
+++ NavMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webbshopen
+{
+    internal class NavMenu
+    {
+       public static void MainMenu()
+        {
+            int mainMenu = 0;//placeholder
+            switch (mainMenu)
+            {
+                case 0:
+                    mainMenu = 0;
+                    break;
+                case 1:
+                    mainMenu = 1;
+                    break;
+                case 2:
+                    mainMenu = 2;
+                    break;
+                case 3:
+                    mainMenu = 3;
+                    break;//placeholders
+            }
+
+            Console.WriteLine("menu");
+            Console.WriteLine();
+            Console.WriteLine("0. hem  1. Kategorier 2. logga in 3. om oss");
+        }
+    }
+}
