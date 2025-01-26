d539bffe9d967e55cca8ff4df58219ca7326295d
Richard Trygg Svartling <tryggrichard@gmail.com>
2025-01-27 00:25:28 +0100

    Ram för kund vagn och produkt sidan

---
+++ SQL/Kundvagn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webbshopen.SQL
+{
+    internal class Kundvagn
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int KategorierId { get; set; }
+
+        public int? Pris { get; set; }
+
+        public int? Antal { get; set; }
+        
+
+        public string? Levrantör { get; set; }
+    }
+}
